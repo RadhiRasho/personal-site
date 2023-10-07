f06dae9a6d38bb68ba7bccd8c1734520d167c59d
Radhi Rasho <Radhi.H@outlook.com>
2023-10-07 22:45:02 +0000

    added other information as well as fixed slight transition delay for navigation menu (desktop)

--- components/about-me.tsx
+++ components/about-me.tsx
@@ -12,13 +12,19 @@ export function AboutMe() {
 			<div className="flex sm:flex-row flex-col min-h-fit justify-between gap-10 mb-4 bg-gradient-conic">
 				<Image className="rounded-full w-72" loading="lazy" alt="Radhi Rasho" src={RadhiRasho} />
 				<div className="flex flex-col items-center justify-center gap-1">
-					<h1 className="text-4xl font-bold">Radhi Rasho</h1>
-					<h2 className="text-xl font-semibold bg-gradient-to-r from-red-500 to-blue-500 bg-clip-text text-transparent">
+					<h1 className="text-3xl font-bold">Radhi Rasho</h1>
+					<h2 className="text-xl font-semibold bg-gradient-to-r from-blue-600 to-red-600 dark:from-red-600 dark:to-blue-600 bg-clip-text text-transparent">
 						Software Engineer
 					</h2>
 				</div>
 			</div>
-			<Accordion defaultValue={"About Me"} collapsible type="single" className="w-10/12 border rounded-lg px-2 mb-4">
+			<Accordion
+				autoFocus
+				defaultValue={"About Me"}
+				collapsible
+				type="single"
+				className="w-10/12 border rounded-lg px-2 mb-4"
+			>
 				<AccordionItem value="About Me">
 					<AccordionTrigger className="hover:text-red-600 font-bold focus:text-red-600 focus-within:text-red-600">
 						About Me
@@ -46,6 +52,11 @@ export function AboutMe() {
 							working out, or spending time with friends and family.
 							<br />
 							<br />
+							In my free time, I like working on computers, such as building custom PC&apos;s, or repairing already
+							built ones with new components. I find it interested how different parts work together, to do amazing
+							things.
+							<br />
+							<br />
 							Feel free to check out my&nbsp;
 							<Link className="dark:text-gray-200 hover:text-red-600" href="/projects">
 								<span className="underline hover:text-red-600">projects</span>
@@ -65,16 +76,17 @@ export function AboutMe() {
 					<AccordionContent className="">
 						<div className="flex flex-col gap-2 dark:text-gray-300">
 							<div className="text-md font-bold underline hover:text-red-600">
-								<Link as="https://www.sandhills.com/" href="https://www.sandhills.com/">
+								<Link as="https://www.sandhills.com/" target="_blank" href="https://www.sandhills.com/">
 									Sandhills Global
 								</Link>
 							</div>
-							<div className="text-xs italic text-gray-600 font-semibold dark:text-gray-400">
-								Software Engineer<span className="text-xs"> - 2021 to Present</span>
+							<div className="text-xs italic font-semibold dark:text-gray-300">
+								Software Engineer<span className="text-xs dark:text-gray-300 text-gray-900"> - 2021 to Present</span>
 								<br />
-								Software Engineer Intern<span className="text-xs"> - 2020 to 2021</span>
+								Software Engineer Intern
+								<span className="text-xs dark:text-gray-300 text-gray-900"> - 2020 to 2021</span>
 							</div>
-							<div className="text-sm">
+							<div className="text-sm dark:text-gray-300">
 								I first started off as an intern at Sandhills Global in Summer of 2020, where I worked on a small
 								projects that allowed me to learn the ropes of the company&apos;s development process. After my
 								internship, I was offered a full time position as a Software Engineer.
@@ -83,7 +95,11 @@ export function AboutMe() {
 								During my time at Sandhills Global, I&apos;ve had the opportunity to work on two significant projects:
 							</div>
 							<div className="dark:text-gray-300">
-								<Link className="text-sm font-bold  underline hover:text-red-600" href={"https://www.fr8star.com"}>
+								<Link
+									className="text-sm font-bold  underline hover:text-red-600"
+									target="_blank"
+									href={"https://www.fr8star.com"}
+								>
 									FR8Star
 								</Link>
 								&nbsp;
@@ -92,7 +108,11 @@ export function AboutMe() {
 								</span>
 								<br />
 								<br />
-								<Link className="text-sm font-bold  underline hover:text-red-600" href={"https://www.gocurrency.com/"}>
+								<Link
+									className="text-sm font-bold  underline hover:text-red-600"
+									target="_blank"
+									href={"https://www.gocurrency.com/"}
+								>
 									GoCurrency
 								</Link>
 								&nbsp;
