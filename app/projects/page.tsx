03812c31668560833c36c02b9b011aad07824b15
Radhi Rasho <Radhi.h@outlook.com>
2023-10-30 00:44:32 +0000

    Projects page now has some stuff to display instead of comming soon

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,29 @@
+import { Project } from "@/components/project";
+import devSite from "@/public/radhi-rasho.dev.jpeg";
+
 export default function Projects() {
+	const projects = [
+		{
+			title: "Personal Site",
+			description:
+				"This is what you are viewing currently, a project to show off stuff I like to do, and stuff I've built over time.",
+			tags: ["Next.js", "Vercel", "GitHub", "Node.js", "React", "TailwindCSS", "TypeScript"],
+			image: devSite,
+			href: "https://github.com/radhirasho/personal-site",
+		},
+		{
+			title: "Meme Sender",
+			description:
+				"Small project that I did to simply grab a meme from the publically avaliable API by Dev Daksan on GitHub and sends it via Email at a scheduled time everyday. It creates a system tray icon on Windows mainly with a few options that you can run such as sending a meme on demand, or changing the time of the scheduled meme.",
+			tags: ["Python", "GitHub", "Windows", "System Tray"],
+			href: "https://github.com/radhirasho/DailyMeme",
+		},
+	];
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<div>Coming Soon...</div>
+		<main className="flex min-h-full flex-col items-center mt-4 justify-between">
+			{projects.map((project, index) => (
+				<Project key={index} {...project} />
+			))}
 		</main>
 	);
 }
---
+++ components/project.tsx
@@ -0,0 +1,52 @@
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
+import { Badge } from "./ui/badge";
+
+type ProjectProps = {
+	title: string;
+	description: string;
+	image?: StaticImageData;
+	href?: string;
+	tags?: string[];
+};
+
+export function Project({ title, description, image, href, tags }: ProjectProps) {
+	return (
+		<div className="flex justify-between overflow-hidden transition-all duration-75 delay-0 rounded-xl w-[95vw] xl:w-[75vw] h-fit mb-2 border-2 dark:border-white border-black">
+			<div className="flex relative w-full justify-between gap-2">
+				<div
+					className={`flex flex-col items-center md:items-start w-full h-fit z-50 md:bg-gradient-to-r dark:from-primary-foreground from-white ${
+						image ? "from-[68%]" : "from-100%"
+					} p-2`}
+				>
+					{href ? (
+						<Link href={href} className="text-lg font-bold underline hover:text-red-500">
+							{title}
+						</Link>
+					) : (
+						<p className="text-lg font-bold">{title}</p>
+					)}
+					<p className="text-md w-10/12">{description}</p>
+					<br />
+					<div className="w-fit">
+						<div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-1">
+							{tags?.map((tag, index) => (
+								<Badge className="flex justify-center !text-xs" key={index}>
+									{tag}
+								</Badge>
+							))}
+						</div>
+					</div>
+				</div>
+				{image && (
+					<Image
+						src={image}
+						alt={title}
+						priority
+						className="hidden dark:bg-primary md:flex w-3/6 lg:w-2/6 h-full absolute right-0"
+					/>
+				)}
+			</div>
+		</div>
+	);
+}
