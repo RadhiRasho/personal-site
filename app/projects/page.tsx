df54a4e28270ecc2c03de82f1f29e5287b0ff78d
Radhi Rasho <Radhi.h@outlook.com>
2024-05-30 15:38:06 +0000

    added new project

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,6 +2,7 @@ import { Project } from "@/components/project";
 import DailyMeme from "@/public/DailyMeme.png";
 import TodoCLI from "@/public/Todo-CLI.png";
 import MySite from "@/public/radhi-rasho.dev.jpeg";
+import GermanQuiz from "@/public/GermanQuiz.png"
 import type { ProjectProps } from "@/types/project";
 
 export default function Projects() {
@@ -15,6 +16,7 @@ export default function Projects() {
 				"Vercel",
 				"GitHub",
 				"Node.js",
+				"Bun",
 				"React",
 				"TailwindCSS",
 				"TypeScript",
@@ -23,21 +25,28 @@ export default function Projects() {
 			href: "https://github.com/radhirasho/personal-site",
 		},
 		{
+			title: "Todo CLI",
+			description:
+				"Simple Todo CLI tool where you can add, delete, list and prioritize your tasks in a cli. Can be ran on both windows and linux, but I have yet to add auto addition to PATH, will be working on that next",
+			tags: ["Go", "SQLite", "CLI"],
+			image: TodoCLI,
+			href: "https://github.com/radhirasho/todo-cli",
+		},
+		{
+			title: "German Quiz CLI",
+			description: "Simple CLI tool to help me learn German, can be used by anyone to learn German",
+			tags: ["Go", "CLI", "JSON"],
+			image: GermanQuiz,
+			href: "https://github.com/radhirasho/german-quiz-cli",
+		},
+		{
 			title: "Daily Meme",
 			description:
 				"Small project that I did to simply grab a meme from the publically avaliable API by Dev Daksan on GitHub and sends it via Email at a scheduled time everyday. It creates a system tray icon on Windows mainly with a few options that you can run such as sending a meme on demand, or changing the time of the scheduled meme.",
 			tags: ["Python", "GitHub", "Windows", "System Tray"],
 			image: DailyMeme,
 			href: "https://github.com/radhirasho/DailyMeme",
-		},
-		{
-			title: "Todo CLI",
-			description:
-				"Simple Todo CLI tool where you can add, delete, list and prioritize your tasks in a cli. Can be ran on both windows and linux, but I have yet to add auto addition to PATH, will be working on that next",
-			tags: ["Go", "SQLite"],
-			image: TodoCLI,
-			href: "https://github.com/radhirasho/todo-cli",
-		},
+		}
 	];
 	return (
 		<main className="flex min-h-full flex-col items-center mt-4 mx-2 justify-between">
