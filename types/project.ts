040cff0a2087a12b430fcc220da0b936e7f40763
Radhi Rasho <Radhi.h@outlook.com>
2024-09-02 03:27:36 +0000

    updates

--- types/project.ts
+++ types/project.ts
@@ -1,7 +1,7 @@
 export type ProjectProps = {
-		title: string;
-		description: string;
-		image?: string;
-		href?: string;
-		tags?: string[];
-	};
+	title: string;
+	description: string;
+	image?: string;
+	href?: string;
+	tags?: string[];
+};
