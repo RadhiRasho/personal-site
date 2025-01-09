750d080e453c50d52945e14f5b473597cfe4b2a4
Radhi Rasho <Radhi.h@outlook.com>
2025-01-09 06:33:56 +0000

    work in progress
    removing unnecessary old stuff

--- app/contact/actions.ts
+++
@@ -1,9 +0,0 @@
-export async function submitCaptcha(token: string) {
-	const captchaRes = await fetch("/api/utils/captcha", {
-		method: "POST",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify({ token }),
-	});
-
-	return captchaRes.json();
-}
