7ad251d71dcb876e163d4a9affe3a7be821d8f10
Radhi Rasho <Radhi.h@outlook.com>
2025-05-10 02:17:37 +0000

    Quality of eyes improvements

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Landing from "@/components/Landing";
-import About from "@/components/about";
+import Background from "@/components/background";
 import Projects from "@/components/projects";
 
 export default function Home() {
 	return (
 		<main className="relative flex h-full min-h-screen w-full min-w-screen flex-col items-center justify-between">
 			<Landing />
-			<About />
+			<Background />
 			<Projects />
 		</main>
 	);
